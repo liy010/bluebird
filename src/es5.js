f65c544fbbc2930d65e82cfbc0c6bd58630c2c97
liyang <1056094880@qq.com>
2018-11-26 12:05:15 +0800

    Update es5.js

--- src/es5.js
+++ src/es5.js
@@ -1,5 +1,12 @@
 var isES5 = (function(){
     "use strict";
+    // 无论是否是严格模式， 在全局作用域下this都指代windows。
+    // 在严格模式下，如果 this 没有被执行上下文（execution context）定义，那它将保持为 undefined。
+//     function f2(){
+//       "use strict"; // 这里是严格模式
+//       return this;
+//     }
+    // this的确应该是undefined，因为f2是被直接调用的，而不是作为对象的属性或方法调用的（如 window.f2()）。
     return this === undefined;
 })();
 
@@ -15,17 +22,18 @@ if (isES5) {
         isES5: isES5,
         propertyIsWritable: function(obj, prop) {
             var descriptor = Object.getOwnPropertyDescriptor(obj, prop);
-            return !!(!descriptor || descriptor.writable || descriptor.set);
+            return !!(!descriptor || descriptor.writable || descriptor.set);  // ** !! ? **
         }
     };
 } else {
     var has = {}.hasOwnProperty;
+    // 获取类型
     var str = {}.toString;
     var proto = {}.constructor.prototype;
 
-    var ObjectKeys = function (o) {
+    var ObjectKeys = function (o) {  // 用于获取对象o自己的属性
         var ret = [];
-        for (var key in o) {
+        for (var key in o) {  // for...in 循环只遍历可枚举属性 会把属性名返回出去
             if (has.call(o, key)) {
                 ret.push(key);
             }
